0d7e97e144cc029724c7b86b370d916f1b6b3fc5
hefei.cao <hefei.cao@xiaoyangedu.com>
2024-06-06 12:19:55 +0800

    build: 初始化项目

---
+++ README.md
@@ -0,0 +1,33 @@
+
+
+conda create --name tradingview_ta python=3.10
+
+conda activate tradingview_ta
+
+
+
+ ### Python 依赖:
+
+ ```
+ pip install -r requirements.txt 
+ ```
+
+
+### 本地运行
+```
+$ python main.py
+
+```
+
+
+### 生成依赖文件（一般不需要执行）
+
+```
+pip list --format=freeze > requirements.txt
+
+```
+
+
+
+
+ 
---
+++ main.py
@@ -0,0 +1,31 @@
+from tradingview_ta import TA_Handler, Interval, Exchange
+from flask import Flask, request
+
+app = Flask(__name__)
+@app.route('/api/query', methods=['GET'])
+def query_data():
+    try:
+       symbol = request.args.get("symbol")
+       interval = request.args.get("interval")
+       return {"data": get_data(symbol,interval=interval), "success": True}  
+    except Exception as e:
+        return {"data": {}, "success": False}
+  
+   
+def get_data(symbol,interval='1d'):
+  print(symbol,interval)
+  tesla = TA_Handler(
+    symbol=symbol,
+    screener="crypto",
+    exchange="BINANCE",
+    # interval=Interval.INTERVAL_1_DAY
+    interval=interval
+  )
+  print(tesla.get_analysis().summary)
+  return  tesla.get_analysis().summary
+
+
+
+if __name__ == '__main__':
+    app.run(port=8686)
+    # get_data("BNBUSDT",'1h')
